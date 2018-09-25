012230babc6ee13a741a0cd325377f0e47b66ceb
Felix Ruhnow <felix.ruhnow@tu-dresden.de>
2018-09-25 16:50:46 +1000

    Modified the updater for the source code

--- FiestaUpdater.m
+++ FiestaUpdater.m
@@ -1,151 +1,130 @@
 function FiestaUpdater
 
-%Set current directory for FIESTA
-DirCurrent = [pwd filesep];
-
 %get path where fiesta.m was started
 DirRoot = [fileparts( mfilename('fullpath') ) filesep];
 
-if ~isdeployed
-    warning('off','all');
-    rmpath(genpath(DirCurrent));
-    warning('on','all');
+warning('off','all');
+
+if isfile([DirRoot 'update.zip'])
+    delete([DirRoot 'update.zip']);
 end
 
-%check wether version runs in MatLab or as Redistributable
-if ~isdeployed
-    %Fiesta runs in MatLab - get Fiesta code
-    mode = 'source';
-    archive = '';
-    DirCurrent = DirRoot;
-else
-    if ispc || ismac ||isunix
-        %get computer type
-        mode = computer;
-        archive = ['/' computer];
-    else
-        errordlg('FIESTA for your Operation System is not yet supported','FIESTA Update Error','modal');
-        return;
-    end
+if isfile([DirRoot 'fiesta.ini_backup'])
+    delete([DirRoot 'fiesta.ini_backup']);
 end
 
 %check if FIESTA online is available
-[~,status]=urlread('http://www.bcube-dresden.de/fiesta/uploads/'); %#ok<*ASGLU>
-if status
-    %check if user has access to FIESTA online
-    [index,~]=urlread(['http://www.bcube-dresden.de/fiesta/uploads/archive' archive]); %#ok<*ASGLU>
-else
-    errordlg({'Could not update FIESTA!','','Make sure that your internet is working.','','Support: ruhnow@bcube-dresden.de'},'FIESTA Error','modal');
+disp('Checking available versions - Please wait...');
+link_fiestainfo{1} = 'https://cloudstore.zih.tu-dresden.de/index.php/s/QXWit4TjaHi30T4/download';
+link_fiestainfo{2} = 'http://bcube-dresden.de/fileadmin/www.bcube-dresden.de/uploads/diez/fiesta/fiestainfo.json';
+link_fiestainfo{3} = 'http://bcube-dresden.de/fileadmin/www.bcube-dresden.de/uploads/diez/fiesta/fiestainfo.json';
+online_num = zeros(size(link_fiestainfo));
+fiesta_info = [];
+opt = weboptions('ContentType','json');
+for n = 1:numel(link_fiestainfo)
+    try
+        info = webread(link_fiestainfo{n},opt);
+    catch
+        info = [];
+    end
+    if isfield(info,'CurrentFiestaVersion')
+        online_version = info.CurrentFiestaVersion';
+        online_num(n) = online_version(1)*100 + online_version(2)*10 + online_version(3);
+        if online_num(n) == max( online_num )
+           fiesta_info = info;
+        end
+    end
+end
+
+list = ListFiestaVersion(fiesta_info);
+
+
+urlzip = GetUserVersion(list,fiesta_info.DownloadSource);
+disp('Downloading update - Please wait...');
+outfilename = websave([DirRoot 'update.zip'],urlzip);
+
+if isempty(outfilename)
+    %FIESTA online files are available but wrong version input
+    errordlg('Latest FIESTA version corrupted','FIESTA Update Error','modal');
     return;
 end
-
-
-list = ListFiestaVersion(index);
-version = GetUserVersion(list);
-
-if strcmp(version,'latest')
-    [index,~] = urlread('http://www.bcube-dresden.de/fiesta/uploads/readme.txt');
-    online_version = native2unicode(index(66:74),'UTF-8');
-else
-    online_version = version;
-end
-
-file_id = fopen([DirCurrent 'readme.txt'], 'r'); 
-if file_id ~= -1
-    index = fgetl(file_id);
-    local_version = index(66:74);
-    fclose(file_id); 
-    if str2double(online_version(4))>str2double(local_version(4))
-        % install new or update new MCR 
-    end
-end
-    
-if strcmp(version,'latest')
-    %get newest version
-    urlzip =['http://www.bcube-dresden.de/fiesta/uploads/FIESTA(' mode ').zip']; 
-    [index,status] = urlread(urlzip);                    
-    if ~status
-        %FIESTA online files are available but wrong version input
-        errordlg('Latest FIESTA version corrupted','FIESTA Update Error','modal');
-        return;
-    end
-else
-    %get version x.xx.xxxx
-    urlzip=['http://www.bcube-dresden.de/fiesta/uploads/archive' archive '/' version '.zip'];                
-    %check if FIESTA version x.xx.xxxx is available
-    [index,status] = urlread(urlzip);  
-    if ~status
-        %FIESTA online files are available but wrong version input
-        errordlg(['Version ' version ' corrupted'],'FIESTA Update Error','modal');
-        return;
-    end
-end
-
+  
 RestoreFiestaIni = false;
 
-files = dir(DirCurrent);
-
+files = dir(DirRoot);
+disp('Removing old version - Please wait...');
 for n = 1:length(files)
     if ~files(n).isdir 
-        if isempty(strfind(files(n).name,'fiesta.ini'))
-            delete([DirCurrent files(n).name]);
-        else
-            movefile([DirCurrent 'fiesta.ini'],[DirCurrent 'fiesta.ini_backup']);
+        if contains(files(n).name,'fiesta.ini')
+            movefile([DirRoot 'fiesta.ini'],[DirRoot 'fiesta.ini_backup']);
             RestoreFiestaIni = true;
+        elseif ~contains(files(n).name,'update.zip')
+            delete([DirRoot files(n).name]);
         end
     else
-        if strcmp(mode,'source') && isempty(strfind(files(n).name,'.'))
-            rmdir([DirCurrent files(n).name],'s');
+        if ~contains(files(n).name,'.')
+            rmpath(genpath([DirRoot files(n).name]));
+            rmdir([DirRoot files(n).name],'s');   
         end
     end
 end
-
-filenames = unzip(urlzip,DirCurrent);
+disp('Extracting new version - Please wait...');
+filenames = unzip(outfilename,DirRoot);
 for i=1:length(filenames)
     fileattrib(filenames{i},'+w');
 end
 if RestoreFiestaIni
-    delete([DirCurrent 'fiesta.ini']);
-    movefile([DirCurrent 'fiesta.ini_backup'],[DirCurrent 'fiesta.ini']);
-end
-msgbox('FIESTA Update complete - please restart FIESTA','FIESTA Update');
-
-function list = ListFiestaVersion(index)
-list=[];
-%get online version of FIESTA
-index = native2unicode(index,'UTF-8');
-p = strfind(index,'<hr>');
-str = index(p(1)+4:p(2)-1);
-k = find( double(str) == 10 );
-start = 1;
-p = 1;
-for n = 1:length(k)
-    if ~isempty(strfind(str(start:k(n)),'.zip'))
-        h_str = strrep(str(start:k(n)-1),'.zip</a>','');
-        ps = strfind(h_str,'.zip">');
-        list{p} = h_str(ps(1)+6:end);         %#ok<AGROW>
-        start = k(n)+1;
-        p=p+1;
+    try
+        file_id = fopen([DirRoot 'fiesta.ini_backup'],'r');
+        Config = jsondecode(fread(file_id,'*char'));
+        fclose(file_id);
+        if ~isempty(Config)
+            delete([DirRoot 'fiesta.ini']);
+            movefile([DirRoot 'fiesta.ini_backup'],[DirRoot 'fiesta.ini']);
+        else
+            disp('Using new fiesta.ini');
+            delete([DirRoot 'fiesta.ini_backup']);
+        end
+    catch
+        if isfile([DirRoot 'fiesta.ini_backup'])
+            disp('Using new fiesta.ini');
+            delete([DirRoot 'fiesta.ini_backup']);
+        end
     end
 end
+if isfile([DirRoot 'update.zip'])
+    delete([DirRoot 'update.zip']);
+end
+warning('on','all');
+disp('Update complete - restart FIESTA');
+msgbox('FIESTA Update complete - please restart FIESTA','FIESTA Update');
 
-function version = GetUserVersion(list)
+function list = ListFiestaVersion(info)
+archive = info.ArchiveSource;
+list = cell(numel(archive),2);
+for n = 1:numel(archive)
+    version = archive(n).Version;
+    list{n,1} = [num2str(version(1)) '.' num2str(version(2)) '.' num2str(version(3))];
+    list{n,2} = archive(n).Download;
+end
+
+function version = GetUserVersion(list,latest_link)
 hVersionDialog = dialog('Name','FIESTA Update','Units','normalized');
-uicontrol('Parent',hVersionDialog,'Units','normalized','Position',[0.1 0.75 0.8 0.2],'Style','pushbutton','String','Get latest FIESTA version','FontSize',12,'FontWeight','bold','UserData','latest','Callback',@doCallback);
+uicontrol('Parent',hVersionDialog,'Units','normalized','Position',[0.1 0.75 0.8 0.2],'Style','pushbutton','String','Get latest FIESTA version','FontSize',12,'FontWeight','bold','UserData',latest_link,'Callback',@doCallback);
 uicontrol('Parent',hVersionDialog,'Units','normalized','Position',[0.1 0.6 0.8 0.05],'Style','text','String','Available FIESTA version','FontSize',12);
-uicontrol('Parent',hVersionDialog,'Units','normalized','Position',[0.1 0.2 0.8 0.4],'Tag','lArchievedVersion','Style','listbox','String',list,'FontSize',12);
-uicontrol('Parent',hVersionDialog,'Units','normalized','Position',[0.1 0.05 0.8 0.1],'Style','pushbutton','String','Get archieved FIESTA version','FontSize',12,'UserData','archieved','Callback',@doCallback);
+uicontrol('Parent',hVersionDialog,'Units','normalized','Position',[0.1 0.2 0.8 0.4],'Tag','lArchievedVersion','Style','listbox','String',list(:,1),'FontSize',12);
+uicontrol('Parent',hVersionDialog,'Units','normalized','Position',[0.1 0.05 0.8 0.1],'Style','pushbutton','String','Get archieved FIESTA version','FontSize',12,'UserData',list,'Callback',@doCallback);
 uiwait(hVersionDialog);
 version = get(hVersionDialog,'UserData');
 delete(hVersionDialog);
 
-function doCallback(obj, evd) %#ok
-if strcmp(get(obj,'UserData'),'latest')
-  set(gcbf,'UserData','latest');
+function doCallback(obj, ~) 
+data = get(obj,'UserData');
+if ~iscell(data)
+    set(gcbf,'UserData',data);    
 else
-  h = findobj('Tag','lArchievedVersion');
-  str = get(h,'String');
-  n = get(h,'Value');
-  set(gcbf,'UserData',str{n}(1:9));
+    h = findobj('Tag','lArchievedVersion');
+    n = get(h,'Value');
+    set(gcbf,'UserData',data{n,2});
 end
 uiresume(gcbf);
